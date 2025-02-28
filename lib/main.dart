0731b19321e8d1c108fe02add7b333d68a79a26d
CelineSelke1 <celineselke@outlook.com>
2025-02-28 13:43:26 -0500

    Add addition, subtraction, multiplication, division, buttons without layout

---
+++ lib/main.dart
@@ -0,0 +1,222 @@
+import 'dart:collection';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculator',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Calculator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var total = 0.0;
+  String totalString = "0";
+  String operator = "start";
+  Queue operand = Queue();
+  int powerCount = -1;
+  bool decimal = false;
+
+  void calculate(){
+      total = 0;
+  }
+
+  void pushOperand(double number){
+    setState(() {
+      if(operator == "="){
+        operand.removeFirst();
+        operator = "start";
+      }
+      else if(operand.isEmpty == false){
+        if(operator == "start" && decimal == false){
+          operand.addFirst(number + (10 * operand.removeFirst())); 
+        }
+        else{
+          if(decimal == true){
+            operand.addFirst(operand.removeFirst() + (number * pow(10, powerCount)));
+            powerCount--;
+          }
+          else{
+            operand.addFirst(number);
+
+          }
+        }
+      }
+      else{
+          if(decimal == true){
+            operand.addFirst(operand.removeFirst() + (number * pow(10, powerCount)));
+            powerCount--;
+          }
+          else{
+            operand.addFirst(number);
+          }
+      }
+
+    });
+  
+  }
+
+  void calculateTotal(){
+      if (operator == "+"){
+          setState(() {
+            total = operand.removeFirst() + operand.removeFirst();
+          });
+      }
+      if (operator == "-"){
+          setState(() {
+            total = operand.removeLast() - operand.removeLast();
+          });
+      }
+      if (operator == "/"){
+          setState(() {
+            double dividend = operand.removeLast();
+            if(operand.first == 0.0){
+               totalString = "Divide By Zero Error";
+            }
+            else{
+               total = dividend / operand.removeLast();
+            }
+          });
+      }
+      if (operator == "*"){
+          setState(() {
+            total = operand.removeFirst() * operand.removeFirst();
+          });
+      } 
+
+      decimal = false;
+
+      operand.addFirst(total);
+
+      setTotalString();
+
+  }
+
+  void setTotalString(){
+    setState(() {
+      if(total % 1 == 0){
+          totalString = total.toString().split('.')[0];
+      }
+      else{
+        var decimalPlaces = total.toString().split('.')[1].length;
+        if(decimalPlaces < 6){
+          totalString = total.toStringAsPrecision(decimalPlaces+1);
+        }
+        else{
+          totalString = total.toStringAsPrecision(7);
+        }
+      }
+
+    });
+
+  }
+
+  void setOperator(String pushedOperator){
+      setState(() {
+
+        operator = pushedOperator;
+        powerCount = -1;
+
+        if(decimal == true){
+          decimal = false;
+        }
+      
+      });
+  }
+
+  void startDecimalOperation(){
+      setState(() {
+        decimal = true;
+      });
+  }
+
+  void reset(){
+    setState(() {
+      total = 0;
+      totalString = "0";
+      operator = "start";
+      while(operand.isEmpty == false){
+        operand.removeFirst();
+      }
+    });
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Text(
+              totalString,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text("{$operand}, $operator"),
+
+            ElevatedButton(onPressed: () => {setOperator("+")}, child: Text("+")),
+            ElevatedButton(onPressed: () => {setOperator("-")}, child: Text("-")),
+            ElevatedButton(onPressed: () => {setOperator("*")}, child: Text("x")),
+            ElevatedButton(onPressed: () => {setOperator("/")}, child: Text("÷")),
+            ElevatedButton(onPressed: () => {startDecimalOperation()}, child: Text(".")),
+            ElevatedButton(onPressed: () => {calculateTotal()}, child: Text("=")),
+            ElevatedButton(onPressed: () => {pushOperand(1.0)}, child: Text("1")),
+            ElevatedButton(onPressed: () => {pushOperand(2.0)}, child: Text("2")),
+            ElevatedButton(onPressed: () => {pushOperand(3.0)}, child: Text("3")),
+            ElevatedButton(onPressed: () => {pushOperand(4.0)}, child: Text("4")),
+            ElevatedButton(onPressed: () => {pushOperand(5.0)}, child: Text("5")),
+            ElevatedButton(onPressed: () => {pushOperand(6.0)}, child: Text("6")),
+            ElevatedButton(onPressed: () => {pushOperand(7.0)}, child: Text("7")),
+            ElevatedButton(onPressed: () => {pushOperand(8.0)}, child: Text("8")),
+            ElevatedButton(onPressed: () => {pushOperand(9.0)}, child: Text("9")),
+            ElevatedButton(onPressed: () => {pushOperand(0.0)}, child: Text("0")),
+          
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: reset,
+        tooltip: 'Reset',
+        child: const Text("Reset"),
+      ),
+    );
+  }
+}
